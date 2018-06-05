12aca97c72ae26cafd363afe0d18be6b185432ee
ariawanzero <ariawan.zero@gmail.com>
2018-06-05 10:01:14 +0700

    change state open on webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,7 @@ module.exports = {
         contentBase: path.join(__dirname, "dist"),
         compress: true,
         port: 9000,
-        stats: "errors-only",
-        open: true
+        stats: "errors-only"    
     },
     plugins: [
         new HtmlWebpackPlugin({ 
